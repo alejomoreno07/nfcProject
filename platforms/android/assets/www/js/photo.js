2d2b703f9ce2d538f671fc0c43bc8f514c8bc7af
Fkiller17 <frankieghost360@gmail.com>
2018-01-28 19:20:36 +0100

    Draggable and Clickable
    Just have to fix position of the circle in the drag.

--- platforms/android/assets/www/js/photo.js
+++ platforms/android/assets/www/js/photo.js
@@ -37,22 +37,119 @@ var app = {
    
 };
 
+    var mouseEventTypes = {
+    touchstart : "mousedown",
+    touchmove : "mousemove",
+    touchend : "mouseup"
+    };
 
- 
+    for (originalType in mouseEventTypes) {
+    document.addEventListener(originalType, function(originalEvent) {
+        event = document.createEvent("MouseEvents");
+        touch = originalEvent.changedTouches[0];
+        event.initMouseEvent(mouseEventTypes[originalEvent.type], true, true,
+                window, 0, touch.screenX, touch.screenY, touch.clientX,
+                touch.clientY, touch.ctrlKey, touch.altKey, touch.shiftKey,
+                touch.metaKey, 0, null);
+        originalEvent.target.dispatchEvent(event);
+    });
+}
+ function myDrag(){
+
+    var circle = document.getElementById('divCircle');
+    var divGrande = document.getElementById('divImagePhoto');
+    divGrande.append(circle)
+    circle.draggable();
+ }
+
+ function moveCircle(event , circle){
+    
+    circle.style.visibility = "visible";
+    circle.position= "absolute";
+    var pic = document.getElementById('myImage');
+    var altezza = pic.height;
+    var larghezza = pic.width;
+
+    var x = event.clientX;
+    var y = event.clientY;
+    if(x>=(larghezza-50))
+        var left = larghezza -50;
+    else
+        var left = x - 50;
+
+    if(y>=(altezza-125))
+        var top = altezza-125;
+    else
+        var top = y - 125;
+
+    circle.style.left= left+"px";
+    circle.style.top= top+"px";
+    
+
+    document.getElementById("debug").innerHTML = "x "+x+" ,y "+y;
+}
+
+
 function cameraTakePicture() { 
    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,  destinationType: Camera.DestinationType.DATA_URI });  
    
    
     function onSuccess(imageURI) {
         var image = document.getElementById('myImage');
-        //image.targetWidth=200px; 
-        //image.targetHeight=200px;
         image.src = imageURI;
+        dragElement(document.getElementById('divCircle'));
+        letDrag();
     }
    
    function onFail(message) { 
       alert('Failed because: ' + message); 
    } 
+}
+
+    function letDrag(){
+        console.log("did that letDrag");
+        var drag = document.getElementById('divCircle');
+        drag.draggable();
+    }
+    
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
 app.initialize();
