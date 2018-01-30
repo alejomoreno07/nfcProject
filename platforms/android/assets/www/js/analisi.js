f4def591e3ce4fbbf5fd8cbc22da62ed646501a4
alejomoreno07 <alejomoreno@hotmail.com>
2018-01-30 11:48:42 -0500

    some views

--- platforms/android/assets/www/js/analisi.js
+++ platforms/android/assets/www/js/analisi.js
@@ -1,25 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-
-
-
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,7 +11,7 @@ var app = {
     onDeviceReady: function() {
         app.fillDate('fecha-content');
         app.getInformation();
-        app.setSubmit('send-btn-div');
+        //app.setSubmit('send-btn-div');
     
     },
     fillDate:function(idElement){
@@ -63,7 +41,7 @@ var app = {
             
             if(app.fieldsValidation()){
                 app.sendInformation();
-                window.location.href='analisi.html';   
+                window.location.href='photo.html';   
             }else{
                 alert('All the fields must be filled in order to proceed.');
             }
@@ -80,28 +58,6 @@ var app = {
     },
    
    
-   
 };
-
-
- 
-function cameraTakePicture() { 
-   navigator.camera.getPicture(onSuccess, onFail, { quality: 50, allowEdit:true, destinationType: Camera.DestinationType.DATA_URI });  
-   
-   
-    function onSuccess(imageURI) {
-        var image = document.getElementById('myImage');
-        //image.targetWidth=200px; 
-        //image.targetHeight=200px;
-        image.src = imageURI;
-        var circle = document.getElementById("circleShape");
-        circle.style="display:block;";
-    }
-   function onFail(message) { 
-      alert('Failed because: ' + message); 
-   }
-}
-
-$("#circleShape").draggable();
 
 app.initialize();
--- platforms/android/assets/www/js/photo.js
+++ platforms/android/assets/www/js/photo.js
@@ -31,8 +31,54 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+        app.fillDate('fecha-content');
+        app.getInformation();
+        app.setSubmit('send-btn-div');
     
     },
+    fillDate:function(idElement){
+        var today = new Date();
+        var day   = today.getDate();
+        var month = today.getMonth()+1;
+        var year  = today.getFullYear();
+        var fecha = document.getElementById(idElement);
+        fecha.innerHTML= day+"-"+month+"-"+year;
+    },
+    getInformation:function(){
+        var name  = localStorage.getItem("product_name");
+        var pippo = localStorage.getItem("product_pippo");
+        app.setData('product-name',name);
+        app.setData('product-pippo',pippo);
+
+    },
+    setData:function(idElement, data){
+        var element = document.getElementById(idElement);
+        element.value = data;
+        element.nextElementSibling.className ="active";
+    },
+    setSubmit:function(idElement){
+        var element = document.getElementById(idElement);
+
+        element.addEventListener('click',function(){
+            
+            if(app.fieldsValidation()){
+                app.sendInformation();
+                window.location.href='analisi.html';   
+            }else{
+                alert('All the fields must be filled in order to proceed.');
+            }
+
+        });
+    },
+    sendInformation:function(){
+        // It must be implemented
+    },
+    fieldsValidation:function(){
+       
+        return true;
+
+    },
+   
    
    
 };
@@ -40,7 +86,7 @@ var app = {
 
  
 function cameraTakePicture() { 
-   navigator.camera.getPicture(onSuccess, onFail, { quality: 50,  destinationType: Camera.DestinationType.DATA_URI });  
+   navigator.camera.getPicture(onSuccess, onFail, { quality: 50, allowEdit:true, destinationType: Camera.DestinationType.DATA_URI });  
    
    
     function onSuccess(imageURI) {
@@ -48,11 +94,14 @@ function cameraTakePicture() {
         //image.targetWidth=200px; 
         //image.targetHeight=200px;
         image.src = imageURI;
+        var circle = document.getElementById("circleShape");
+        circle.style="display:block;";
     }
-   
    function onFail(message) { 
       alert('Failed because: ' + message); 
-   } 
+   }
 }
+
+$("#circleShape").draggable();
 
 app.initialize();
